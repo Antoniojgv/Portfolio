0297790f459b8f8c06a988d3d27ee1c40bf7f457
picagantes <antoniojesusgv@gmail.com>
2022-07-05 17:52:54 +0200

    tabla skills añadida con recursos de muestreo a traves de api

---
+++ app/Http/Controllers/API/SkillsController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Skills;
+use Illuminate\Http\Request;
+
+class SkillsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $skills = Skills::all();
+        return response()->json($skills);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'skillName' => 'required',
+            'percent' => 'min:0|max:100'
+        ]);
+
+        $newSkill = new Skills([
+            'skillName' => $request->get('skillName'),
+            'percent' => $request->get('percent'),
+        ]);
+
+        $newSkill->save();
+
+        return response()->json($newSkill);
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $skill = Skills::findOrFail($id);
+        return response()->json($skill);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $skill = Skills::findOrFail($id);
+        $skill->delete();
+        return response()->json($skill::all());
+    }
+}
